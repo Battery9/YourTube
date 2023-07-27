201ac51062daf91d18acdbe7b4d4ac4d48d63af8
Battery9 <abiswas9009@gmail.com>
2023-07-27 12:29:37 +0530

    Implemented hash routers, fixed search page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import { AllContext } from "./context/apiContext";
 import VideoPage from "./components/VideoPage";
 import SearchPage from "./components/SearchPage"
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <AllContext>
-      <BrowserRouter>
+      <HashRouter>
         <div className="flex flex-col h-full">
           <Header />
           <Routes>
             <Route path="/" exact element={<Home />} />
             <Route path="/video/:id" element={<VideoPage />} />
             <Route path="/search/:query" element={<SearchPage />} />
+            <Route path="*" element={<Home />} />
           </Routes>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </AllContext>
   );
 }
--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -25,26 +25,40 @@ function SearchPage() {
 
   return (
     <div className="bg-black text-white flex h-full overflow-x-hidden">
-        <LeftNavBar />
+      <LeftNavBar />
       <div className="h-[100%-3rem] w-full overflow-x-hidden m-9 mr-0 scrollbar-hide">
         {searchResult?.map((video) => {
           return (
             <div key={video?.videoId} className="flex my-4">
               <Link to={`/video/${video?.videoId}`}>
-              <div className="h-38 w-48 md:h-36 md:w-52 object-fill flex-shrink-0">
-                <img src={video?.thumbnail[0]?.url} alt={video.title} className="rounded-lg w-full h-full"/>
-              </div>
+                <div className="h-38 w-48 md:h-36 md:w-52 object-fill flex-shrink-0">
+                  <img
+                    src={video?.thumbnail[0]?.url}
+                    alt={video?.title}
+                    className="rounded-lg w-full h-full"
+                  />
+                </div>
               </Link>
               <div className="ml-3">
                 <Link to={`/video/${video?.videoId}`}>
-                <p className="line-clamp-2 md:line-clamp-1 md:text-lg font-medium">{video?.title}</p>
-                <p className="hidden md:block line-clamp-2 font-light">{video?.description}</p>
-                <p className="text-sm">{video?.viewCount} - {video?.publishedText}</p>
+                  <p className="line-clamp-2 md:line-clamp-1 md:text-lg font-medium">
+                    {video?.title}
+                  </p>
+                  <p className="hidden md:block line-clamp-2 font-light">
+                    {video?.description}
+                  </p>
+                  <p className="text-sm">
+                    {video?.viewCount} - {video?.publishedText}
+                  </p>
                 </Link>
                 <div className="flex ml-2 mt-2 gap-2 items-center">
-                  <img src={video?.channelThumbnail[0].url} className="h-9 w-9 rounded-full" alt="video?.channelTitle"/> <span>{video?.channelTitle}</span>
+                  <img
+                    src={video?.channelThumbnail[0].url}
+                    className="h-9 w-9 rounded-full"
+                    alt={video?.channelTitle}
+                  />{" "}
+                  <span>{video?.channelTitle}</span>
                 </div>
-                
               </div>
             </div>
           );
